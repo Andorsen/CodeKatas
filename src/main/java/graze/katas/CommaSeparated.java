8823142ded47a8ad2d1f36ba90803bafd292f385
Graze <Andorsen@live.no>
2021-10-01 14:36:55 +0200

    initial commit

--- src/main/java/graze/katas/CommaSeparated.java
+++ src/main/java/graze/katas/CommaSeparated.java
@@ -1,5 +1,40 @@
 package graze.katas;
 
-public class CommaSeparated
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class CommaSeparated
 {
+    private static StringBuilder s;
+
+    public static String commaSeparatedString(List<Integer> integerList)
+    {
+        s = new StringBuilder();
+        if(integerList.size()>0)
+            s.append(intToStringWithPrefix(integerList.get(0)));
+
+
+        for (int i = 1; i < integerList.size(); i++)
+            s.append(intToStringWithPrefixAndComma(integerList.get(i)));
+        return s.toString();
+    }
+
+    private static String intToStringWithPrefixAndComma(int i)
+    {
+        return "," + intToStringWithPrefix(i);
+    }
+
+    private static String intToStringWithPrefix(int i)
+    {
+        char prefix = i % 2 == 0 ? 'e' : 'o';
+        return ""+ prefix + i;
+    }
+
+    public static String commaSeparatedStringUsingStream(List<Integer> integerList)
+    {
+        return integerList
+                .stream()
+                .map(CommaSeparated::intToStringWithPrefix)
+                .collect(Collectors.joining(","));
+    }
 }
--- src/test/java/CommaSeparatedTest.java
+++ src/test/java/CommaSeparatedTest.java
@@ -7,21 +7,94 @@
  */
 
 import graze.katas.CommaSeparated;
+import static graze.katas.CommaSeparated.commaSeparatedString;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class CommaSeparatedTest
 {
-    @Test
-    void canCreateInstance()
+    List<Integer> integerList;
+
+    private void addToList(int... ints)
     {
-        CommaSeparated commaSeparated = new CommaSeparated();
-        assertNotNull(commaSeparated);
+        for(int i : ints)
+            integerList.add(i);
+    }
+    @BeforeEach
+    void setUp()
+    {
+        integerList = new ArrayList<>();
     }
 
     @Test
-    void name()
+    void emptyList_returnsEmptyString()
     {
+        assertEquals("", commaSeparatedString(integerList));
+    }
+
+    @Test
+    void listOf1_returnsNonEmptyString()
+    {
+        integerList.add(1);
+        assertNotEquals("", commaSeparatedString(integerList));
+    }
+
+    @Test
+    void listOf1_returns_o1()
+    {
+        integerList.add(1);
+        assertEquals("o1", commaSeparatedString(integerList));
+    }
+
+    @Test
+    void listOfTwoInts_returnsWithOneComma()
+    {
+        integerList.add(1);
+        integerList.add(1);
+        assertEquals("o1,o1", commaSeparatedString(integerList));
+    }
+
+    @Test
+    void listOf2_returns_e2()
+    {
+        integerList.add(2);
+        assertEquals("e2", commaSeparatedString(integerList));
+    }
+
+    @Test
+    void listOf135()
+    {
+        addToList(1, 3, 5);
+        assertEquals("o1,o3,o5", commaSeparatedString(integerList));
+    }
+
+    @Test
+    void listOf246()
+    {
+        addToList(2, 4, 6);
+        assertEquals("e2,e4,e6", commaSeparatedString(integerList));
+    }
+
+    @Test
+    void listOf1234567890()
+    {
+        addToList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+        assertEquals("o1,e2,o3,e4,o5,e6,o7,e8,o9,e0",
+                commaSeparatedString(integerList));
+    }
+
+    @Test
+    void listOf1234567890_usingStream()
+    {
+        addToList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+        assertEquals("o1,e2,o3,e4,o5,e6,o7,e8,o9,e0",
+                CommaSeparated.commaSeparatedStringUsingStream(integerList));
     }
 }
